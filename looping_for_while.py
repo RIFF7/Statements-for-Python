e163bff590c4fa22fe8216748876fe83959f6123
RIIF <40139992+RIFF7@users.noreply.github.com>
2023-07-17 11:57:35 +0700

    Create looping_for_while.py
    Make easry looping for WHILE statement

---
+++ looping_for_while.py
@@ -0,0 +1,6 @@
+while True:
+  print("This Program will you make looping if you say NO from question")
+  question = input("You will next step? ")
+  if question != "no":
+    break
+print("You can STOP program!")
