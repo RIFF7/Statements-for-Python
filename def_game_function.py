e51c9ee5bde475aee351c874ce163eb1d3f44842
RIIF <40139992+RIFF7@users.noreply.github.com>
2023-07-24 16:57:56 +0700

    Update def_game_function.py
    Update Game Build while nested

--- def_game_function.py
+++ def_game_function.py
@@ -20,9 +20,9 @@ def generateStrength():
     result = int((roll_side_6 * roll_side_12) / 2 + 12)
     return result
 
-close = "yes"
+#close = "yes"
 
-while close == "yes":
+while True:
     #clear DATA
     os.system("cls")
     #Show Menu
@@ -36,16 +36,88 @@ while close == "yes":
     #Take user Input
     userInput = int(input())
     if userInput == 1:
-        character = input("Name Your Legend: ")
-        typeCharacter = input("Character Type (Human, Elf, Wiard, Orc): ")
-        print(character)
-        healt = str(generateHealt())
-        print("HEALT:", healt)
-        strength = str(generateStrength())
-        print("STRENGTH:", strength)
+        character1 = input("Name Your Legend: ")
+        typeCharacter1 = input("Character Type (Human, Elf, Wiard, Orc): ")
+        print(character1)
+        healt1 = generateHealt()
+        print("HEALT:", healt1)
+        strength1 = generateStrength()
+        print("STRENGTH:", strength1)
+        
+        print()
+        print("Who are they battling?")
+        print()
+        
+        character2 = input("Name Your Legend: ")
+        typeCharacter2 = input("Character Type (Human, Elf, Wiard, Orc): ")
+        print(character2)
+        healt2 = generateHealt()
+        print("HEALT:", healt2)
+        strength2 = generateStrength()
+        print("STRENGTH:", strength2)
+        
+        round = 1
+        winner = None
+        
+        while True:
+            time.sleep(1)
+            print()
+            print("⚔️ BATTLE TIME ⚔️")
+            print()
+            print("The battle begins!")
+            print()
+            
+            play1 = rollDice(6)
+            play2 = rollDice(6)
+            
+            difference = abs(strength1 - strength2) + 1
+            
+            if play1 > play2:
+                healt2 -= difference
+                if round == 1:
+                    print(character1, "wins the first blow")
+                else:
+                    print(character1, "wins round", round)
+            elif play2 > play1:
+                healt1 -= difference
+                if round == 1:
+                    print(character2, "wins the first blow")
+                else:
+                    print(character2, "wins round", round)
+            else:
+                print("Their swords clash and they draw round", round)
+            
+            print()
+            print(character1)
+            print("HEALT: ", healt1)
+            print()
+            print(character2)
+            print("HEALT: ", healt2)
+            print()
+            
+            if healt1 <= 0:
+                print(character1, "has died!")
+                winner = character2
+                break
+            elif healt2 <= 0:
+                print(character2, "has died!")
+                winner = character1
+                break
+            else:
+                print("And they're both standing for the next round")
+                round += 1
+        print()
         print("May your name go down in Legend...")
     elif userInput == 2:
         exit()
     else:
         continue
-    close = input("Again? ")
+    
+    again = input("Again? ")
+    if again == "NO" or again == "no":
+        print()
+        print("⚔️ BATTLE TIME ⚔️")
+        print()
+        print(winner, "has won in", round, "rounds")
+        break
+    time.sleep(1)
