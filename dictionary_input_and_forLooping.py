821e3296317151781249431b430d5735733a3ac5
RIIF <40139992+RIFF7@users.noreply.github.com>
2023-08-01 00:08:14 +0700

    Create dictionary_input_and_forLooping.py
    Membuat masukan yang ditampung oleh dictionary dan ditampilkan dengan fungsi perulangan FOR

---
+++ dictionary_input_and_forLooping.py
@@ -0,0 +1,21 @@
+""" nameSite = input("Input the website name: ").strip().capitalize()
+url = input("Input the URL: ").strip()
+desCribe = input("Input your a description: ").strip()
+star = input("Input the a star rating out of 5: ")
+
+myCard = {"name": nameSite, "url": url, "describe": desCribe,  "star": star}
+print()
+for name, value in myCard.items():
+    print(f"{name}: {value}") """
+
+#Create Simple Code
+website = {"name": None, "url": None, 
+           "desc": None, "star": None}
+
+for name in website:
+    website[name] = input(f"{name}: ")
+
+print()
+
+for name, value in website.items():
+    print(f"{name}: {value}")
