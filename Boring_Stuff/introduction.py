2ac706b4a047c1ab1620fd19fefedba64d18c887
RIFF7 <syarifudinjaelani@gmail.com>
2024-03-19 16:19:36 +0700

    Modifikasi code pertama, hasil masih tetap sama

--- Boring_Stuff/introduction.py
+++ Boring_Stuff/introduction.py
@@ -7,13 +7,26 @@
     Maka yang perlu diperhatikan saat ini adalah, pastikan untuk menuliskan file
     secara lengkap beserta nama foldernya.
 '''
-passwordFile = open('F:\Fokus-Python\GitHub\Statements-for-Python\Boring_Stuff\SecretPasswordFile.txt')
-secretPassword = passwordFile.read()
-print('Enter Your Password.')
-typePassword = input()
-if typePassword == secretPassword:
+# passwordFile = open('F:\Fokus-Python\GitHub\Statements-for-Python\Boring_Stuff\SecretPasswordFile.txt')
+# secretPassword = passwordFile.read()
+# print('Enter Your Password.')
+# typePassword = input()
+# if typePassword == secretPassword:
+#     print('Access Granted!')
+#     if typePassword == '12345':
+#         print('That password is one that an idiot puts on their luggage.')
+# else:
+#     print('Access Denied')
+
+#--------------------------------------------------------------------------------------------------------------
+    
+# Modifikasi code 1
+passwordFile1 = open('F:\Fokus-Python\GitHub\Statements-for-Python\Boring_Stuff\SecretPasswordFile.txt')
+secretPassword1 = passwordFile1.read()
+typePassword1 = input('Enter Your Password: ')
+if typePassword1 == secretPassword1:
     print('Access Granted!')
-    if typePassword == '12345':
-        print('That password is one that an idiot puts on their luggage.')
+elif typePassword1 == '12345':
+    print('That password is one that an idiot puts on their luggage.')
 else:
-    print('Access Denied')
+    print('Access Denied!')
