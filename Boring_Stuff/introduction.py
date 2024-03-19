3bdd6380429ec4261a2a9a9f82436e461e062c9e
RIFF7 <syarifudinjaelani@gmail.com>
2024-03-19 17:17:47 +0700

    Modifikasi code 2, menggunakan perulangan while

--- Boring_Stuff/introduction.py
+++ Boring_Stuff/introduction.py
@@ -7,13 +7,16 @@
     Maka yang perlu diperhatikan saat ini adalah, pastikan untuk menuliskan file
     secara lengkap beserta nama foldernya.
 '''
-# passwordFile = open('F:\Fokus-Python\GitHub\Statements-for-Python\Boring_Stuff\SecretPasswordFile.txt')
+
+# ORIGINAL
+
+# passwordFile = open('..\Statements-for-Python\Boring_Stuff\SecretPasswordFile.txt')
 # secretPassword = passwordFile.read()
 # print('Enter Your Password.')
 # typePassword = input()
 # if typePassword == secretPassword:
 #     print('Access Granted!')
-#     if typePassword == '12345':
+#     if typePassword == '12345': # Hasilnya tidak sesuai yang diharapkan
 #         print('That password is one that an idiot puts on their luggage.')
 # else:
 #     print('Access Denied')
@@ -21,12 +24,31 @@
 #--------------------------------------------------------------------------------------------------------------
     
 # Modifikasi code 1
-passwordFile1 = open('F:\Fokus-Python\GitHub\Statements-for-Python\Boring_Stuff\SecretPasswordFile.txt')
-secretPassword1 = passwordFile1.read()
-typePassword1 = input('Enter Your Password: ')
-if typePassword1 == secretPassword1:
-    print('Access Granted!')
-elif typePassword1 == '12345':
-    print('That password is one that an idiot puts on their luggage.')
-else:
-    print('Access Denied!')
+
+# passwordFile1 = open('..\Statements-for-Python\Boring_Stuff\SecretPasswordFile.txt')
+# secretPassword1 = passwordFile1.read()
+# typePassword1 = input('Enter Your Password: ')
+# if typePassword1 == secretPassword1:
+#     print('Access Granted!')
+# elif typePassword1 == '12345':
+#     print('That password is one that an idiot puts on their luggage.')
+# else:
+#     print('Access Denied!')
+
+#--------------------------------------------------------------------------------------------------------------
+
+# Modifikasi code 2
+
+while True:
+    passwordFile2 = open('..\Statements-for-Python\Boring_Stuff\SecretPasswordFile.txt')
+    secretPassword2 = passwordFile2.read()
+    
+    typePassword2 = input('Enter Your Password: ')
+    if typePassword2 == secretPassword2:
+        print('Access Granted!')
+        break
+    elif typePassword2 == '12345':
+        print('That password is one that an idiot puts on their luggage.')
+    else:
+        print('Access Denied!')
+        break
