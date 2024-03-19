012c71a87353773f420072185136210e66cfdb3f
RIFF7 <syarifudinjaelani@gmail.com>
2024-03-19 19:52:49 +0700

    Pengenalan Aritmatika

---
+++ Boring_Stuff/part1_basics.py
@@ -0,0 +1,43 @@
+# Part 1 BASICS PYTHON
+
+# Penjumlahan
+2 + 2
+
+# Output
+2
+
+# Operator Aritmatika
+# Penjumlahan dan Perkalian (Tanpa Fokus Bilangan) -> Addition and Multiplication
+2 + 3 * 6
+
+'''
+    Addition and Multiplication
+    Penjumlahan dan Perkalian (Dengan Fokus Bilangan -> (Fokus bilangan dalam kurung))
+'''
+(2 + 3) * 6
+
+# Perkalian (Multification)
+48565878 * 578453
+
+# Pangkat (Exponent)
+2 ** 8
+
+# Pembagian Koma (Division)
+23 / 7
+
+# Pembagian Bulat (Integer Division / Floored Quotient)
+23 // 7
+
+# Sisa Hasil Bagi (Modulus / Reminder)
+23 % 7
+
+# Penjumlahan Dengan Test Spasi
+2       +       2
+
+'''
+    Penjumlahan, Pengurangan, Perkalian, Pembagian 
+    dengan fokus bilangan dalam kurung
+'''
+(5 - 1) * ((7 + 1) / (3 - 1))
+
+#--------------------------------------------------------------------------------------------------------------
