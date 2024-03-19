b0ab8bdb9ffb9e066424807db3fdfc14e6ff083f
RIFF7 <syarifudinjaelani@gmail.com>
2024-03-19 16:07:00 +0700

    Update penjelasan

--- Boring_Stuff/introduction.py
+++ Boring_Stuff/introduction.py
@@ -1,4 +1,12 @@
 # Menulis code untuk membaca file berisis password
+'''
+    Untuk membaca sebuah file pada folder, jika muncul error seperti dibawah ini:
+    
+    FileNotFoundError: [Errno 2] No such file or directory: 'SecretPasswordFile.txt'
+    
+    Maka yang perlu diperhatikan saat ini adalah, pastikan untuk menuliskan file
+    secara lengkap beserta nama foldernya.
+'''
 passwordFile = open('F:\Fokus-Python\GitHub\Statements-for-Python\Boring_Stuff\SecretPasswordFile.txt')
 secretPassword = passwordFile.read()
 print('Enter Your Password.')
