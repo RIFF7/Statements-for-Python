5ea11f7bec06b283f18728da1e4a3aa50f5871c8
RIFF7 <syarifudinjaelani@gmail.com>
2024-03-19 20:36:54 +0700

    Membuat program pertama untuk menampilkan nama, panjang karakter nama dan menampilkan umur

--- Boring_Stuff/part1_basics.py
+++ Boring_Stuff/part1_basics.py
@@ -104,3 +104,23 @@ spam
 '''
 spam = 'GoodBye'
 spam
+
+#--------------------------------------------------------------------------------------------------------------
+
+# First Program
+
+# This program says hello and asks for my name
+# Menanyakan Nama
+print('Hello World!')
+print('What is your name? ')
+myName = input()
+print('It is good to meet you, ' + myName)
+
+# Menampilkan panjang karakter nama
+print('The length of your name is:')
+print(len(myName))
+
+# Menanyakan umur
+print('What is your age?')
+myAge = input()
+print('You will be ' + str(int(myAge) + 1) + ' in a year.')
